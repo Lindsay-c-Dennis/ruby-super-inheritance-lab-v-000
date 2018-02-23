2ecbef69d4b957574610ea42fcc07c38b0203bb1
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-02-23 22:39:48 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -2,12 +2,22 @@ class ChattyStudent < Student
   
   def hello
     super
+<<<<<<< HEAD
     puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end
   
   def raise_hand
     10.times do 
       super
+=======
+    "And did you hear about that volcano that erupted it was so cool."
+  end
+  
+  def raise_hand
+    super
+    9.times do 
+      puts "Pick me!"
+>>>>>>> b5607a847bc9be8b4a09622335cf7080240d8771
     end
     end
   
